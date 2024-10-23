aea80741d25889f02f290c5e8169df5f65a3e013
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2024-10-23 11:50:10 +0300

    init project

---
+++ near/README.md
@@ -0,0 +1,4 @@
+Build contract:
+```shell
+cargo build --manifest-path Cargo.toml --target wasm32-unknown-unknown --release
+```
---
+++ near/connector/src/lib.rs
@@ -0,0 +1,35 @@
+use near_plugins::{access_control, AccessControlRole, AccessControllable, Pausable, Upgradable};
+use near_sdk::borsh::BorshDeserialize;
+use near_sdk::near;
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::PanicOnDefault;
+
+#[derive(AccessControlRole, Deserialize, Serialize, Copy, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub enum Role {
+    DAO,
+    PauseManager,
+    UpgradableCodeStager,
+    UpgradableCodeDeployer,
+}
+
+#[near(contract_state)]
+#[derive(Pausable, Upgradable, PanicOnDefault)]
+#[access_control(role_type(Role))]
+#[pausable(manager_roles(Role::PauseManager))]
+#[upgradable(access_control_roles(
+    code_stagers(Role::UpgradableCodeStager, Role::DAO),
+    code_deployers(Role::UpgradableCodeDeployer, Role::DAO),
+    duration_initializers(Role::DAO),
+    duration_update_stagers(Role::DAO),
+    duration_update_appliers(Role::DAO),
+))]
+pub struct BitcoinConnector {}
+
+#[near]
+impl BitcoinConnector {
+    #[init]
+    pub fn new() -> Self {
+        Self {}
+    }
+}
